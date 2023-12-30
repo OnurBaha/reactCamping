01e7212acd0f77a3d48345e97af7d8c0a3078dd7
OnurBaha <koconurbaha@gmail.com>
2023-12-30 20:42:23 +0300

    Added useParams Hook.

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function ProductDetail() {
+    let {id} = useParams()
   return (
-    <div>ProductDetail</div>
+    <div>
+        Ürün : {id}
+    </div>
   )
 }
