b46f3e6f2aabc2aaada7e918db979d6d8f16671b
OnurBaha <koconurbaha@gmail.com>
2024-01-01 18:46:37 +0300

    cartAction and cartReducer Operations Added.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const store = configureStore()
+
+
 root.render(
 
+    <Provider store={store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
 
 );
 
---
+++ src/store/configureStore.js
@@ -0,0 +1,9 @@
+
+import {legacy_createStore as createStore} from "redux";
+import rootReducer from "./rootReducer";
+import { devToolsEnhancer } from "@redux-devtools/extension";
+
+
+export const configureStore =()=>{
+    return createStore(rootReducer,devToolsEnhancer())
+}
